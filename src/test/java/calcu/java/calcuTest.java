1c86ee45a6aff48c17f89a776364d0af85c19253
capaciti <capaciti>
2025-02-11 14:39:14 +0200

    The @Test annotation on the calculator test package file is giving me an error, its either the JUnit dependancy is taking time to read on the pom

--- src/test/java/calcu/java/calcuTest.java
+++ src/test/java/calcu/java/calcuTest.java
@@ -1,4 +1,34 @@
 package calcu.java;
+import static junit.framework.Assert.assertEquals;
+
 
 public class calcuTest {
+    private final Calcu calculator = new Calcu();
+
+    @Test
+    public void testAddTwoIntegers() {
+        assertEquals(3, calculator.add(1, 2));
+        assertEquals(-2, calculator.add(-1, -1));
+    }
+
+    @Test
+    public void testAddMultipleIntegers() {
+        assertEquals(15, calculator.add(1, 2, 3, 4, 5));
+        assertEquals(3, calculator.add(1, 2));
+        assertEquals(-2, calculator.add(-1, -1));
+    }
+
+    @Test
+    public void testMultiplyTwoIntegers() {
+        assertEquals(3, calculator.multiply(1, 3));
+        assertEquals(-3, calculator.multiply(-1, 3));
+    }
+
+    @Test
+    public void testMultiplyMultipleIntegers() {
+        assertEquals(120, calculator.multiply(1, 2, 3, 4, 5));
+        assertEquals(3, calculator.multiply(1, 3));
+        assertEquals(-3, calculator.multiply(-1, 3));
+    }
+
 }
