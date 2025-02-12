1a9bbcf2b13d78cbd28850f43e3fd5c8f3f42ee0
capaciti <capaciti>
2025-02-12 11:32:12 +0200

    structured the code using the Arrange, Act, Assert

--- src/test/java/calcu/java/calcuTest.java
+++ src/test/java/calcu/java/calcuTest.java
@@ -10,28 +10,89 @@ public class calcuTest {
 
     @Test
     public void testAddTwoIntegers() {
-        assertEquals(3, calculator.add(1, 2));
-        assertEquals(-2, calculator.add(-1, -1));
+        // Arrange
+        int num1 = 1;
+        int num2 = 2;
+        int expectedSum1 = 3;
+
+        int num3 = -1;
+        int num4 = -1;
+        int expectedSum2 = -2;
+
+        // Act
+        int result1 = calculator.add(num1, num2);
+        int result2 = calculator.add(num3, num4);
+
+        // Assert
+        assertEquals(expectedSum1, result1);
+        assertEquals(expectedSum2, result2);
     }
 
     @Test
     public void testAddMultipleIntegers() {
-        assertEquals(15, calculator.add(1, 2, 3, 4, 5));
-        assertEquals(3, calculator.add(1, 2));
-        assertEquals(-2, calculator.add(-1, -1));
+        // Arrange
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int expectedSum1 = 15;
+
+        int[] nums2 = {1, 2};
+        int expectedSum2 = 3;
+
+        int[] nums3 = {-1, -1};
+        int expectedSum3 = -2;
+
+        // Act
+        int result1 = calculator.add(nums1);
+        int result2 = calculator.add(nums2);
+        int result3 = calculator.add(nums3);
+
+        // Assert
+        assertEquals(expectedSum1, result1);
+        assertEquals(expectedSum2, result2);
+        assertEquals(expectedSum3, result3);
     }
 
     @Test
     public void testMultiplyTwoIntegers() {
-        assertEquals(3, calculator.multiply(1, 3));
-        assertEquals(-3, calculator.multiply(-1, 3));
+        // Arrange
+        int num1 = 1;
+        int num2 = 3;
+        int expectedProduct1 = 3;
+
+        int num3 = -1;
+        int num4 = 3;
+        int expectedProduct2 = -3;
+
+        // Act
+        int result1 = calculator.multiply(num1, num2);
+        int result2 = calculator.multiply(num3, num4);
+
+        // Assert
+        assertEquals(expectedProduct1, result1);
+        assertEquals(expectedProduct2, result2);
     }
 
     @Test
     public void testMultiplyMultipleIntegers() {
-        assertEquals(120, calculator.multiply(1, 2, 3, 4, 5));
-        assertEquals(3, calculator.multiply(1, 3));
-        assertEquals(-3, calculator.multiply(-1, 3));
-    }
+        // Arrange
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int expectedProduct1 = 120;
 
+        int[] nums2 = {1, 3};
+        int expectedProduct2 = 3;
+
+        int[] nums3 = {-1, 3};
+        int expectedProduct3 = -3;
+
+        // Act
+        int result1 = calculator.multiply(nums1);
+        int result2 = calculator.multiply(nums2);
+        int result3 = calculator.multiply(nums3);
+
+        // Assert
+        assertEquals(expectedProduct1, result1);
+        assertEquals(expectedProduct2, result2);
+        assertEquals(expectedProduct3, result3);
+    }
 }
+
+
